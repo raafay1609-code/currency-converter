33b68a6581fccf8e5634c689f76884f03dfa5df7
raafay ahmed <raafay@arittek.com>
2026-01-03 23:42:42 +0500

    fix: update CORS configuration to allow all origins

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: true,
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
