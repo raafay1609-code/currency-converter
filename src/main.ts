9cfd6ca732243270bb3a14a11d36a81a3155dfd1
raafay ahmed <raafay@arittek.com>
2026-01-04 01:03:39 +0500

    fix: update main.ts to properly configure server listening and add Procfile

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  // await app.listen(process.env.PORT ?? 3000);
+  const port = process.env['PORT'] || 3000;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
