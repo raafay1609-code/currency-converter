36c36dbe9691ad45e59c0abaa81e0e23cee4ba80
raafay ahmed <raafay@arittek.com>
2026-01-03 22:56:51 +0500

    uyuu

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:4200',
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
